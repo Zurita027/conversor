6bf3142ef419be7a26174a6bd65e7cd4e9dc2ef8
Zurita027 <139669047+Zurita027@users.noreply.github.com>
2024-05-10 08:33:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function converter() {
-    
     var temperaturaCelsius = parseFloat(document.getElementById("tempCelsius").value);
     if (!isNaN(temperaturaCelsius)) {
         var temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
-        document.getElementById("resultado").innerHTML =temperaturaFahrenheit.toFixed(2) + "°F";
+        document.getElementById("resultado").innerHTML = temperaturaFahrenheit.toFixed(2) + "°F";
     } else {
         document.getElementById("resultado").innerHTML = "Por favor, insira um valor numérico válido para a temperatura.";
     }
 }
 
-function resetar() {
+function limpar() {
     document.getElementById("tempCelsius").value = "";
     document.getElementById("resultado").innerHTML = "";
-    window.location.reload();
 }
